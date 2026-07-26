64c60713f7d8dbb10203af091472dfb26c5a716e
Icaro Santos <icaro.santosdev@gmail.com>
2026-07-26 10:33:27 -0300

    feat(auth): reject invalid password
    Add password validation to authentication.
    
    * throw InvalidCredentialsException when password does not match
    * use a generic message to avoid revealing which credential is invalid

--- src/main/java/com/icarosantos/helpdesk/common/exception/InvalidCredentialsException.java
+++ src/main/java/com/icarosantos/helpdesk/common/exception/InvalidCredentialsException.java
@@ -2,4 +2,7 @@ package com.icarosantos.helpdesk.common.exception;
 
 public class InvalidCredentialsException extends RuntimeException {
 
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
 }
