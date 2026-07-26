a1ae8a669377411d0b9334f1595626ceeae31f80
Icaro Santos <icaro.santosdev@gmail.com>
2026-07-26 10:59:18 -0300

    refactor(auth): hardcode generic message in InvalidCredentialsException
    Remove duplicated message from exception call sites.
    
    * change InvalidCredentialsException to a no-arg constructor with a fixed generic message
    * update authenticate() and validatePassword() to use the no-arg constructor
    * keep authenticate() behavior unchanged
    * all existing tests remain green

--- src/main/java/com/icarosantos/helpdesk/common/exception/InvalidCredentialsException.java
+++ src/main/java/com/icarosantos/helpdesk/common/exception/InvalidCredentialsException.java
@@ -2,7 +2,7 @@ package com.icarosantos.helpdesk.common.exception;
 
 public class InvalidCredentialsException extends RuntimeException {
 
-    public InvalidCredentialsException(String message) {
-        super(message);
+    public InvalidCredentialsException() {
+        super("Invalid email or password");
     }
 }
